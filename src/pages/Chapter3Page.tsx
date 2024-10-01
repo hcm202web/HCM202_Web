41bba6418864232c0f9750fee7acb20b697f9a83
Nguyễn Công Thịnh <thinh111190@gmail.com>
2024-10-01 15:46:59 +0700

    update-chapter3

--- src/pages/Chapter3Page.tsx
+++ src/pages/Chapter3Page.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+declare global {
+    interface Window {
+      FB: any;
+    }
+  }
 const Chapter3Page: React.FC = () => {
+    const handleTestButtonClick = () => {
+        console.log('Navigating to the test...');
+    };
+    useEffect(() => {
+        // Kiểm tra nếu SDK của Facebook đã được tải
+        if (window.FB) {
+          window.FB.XFBML.parse();
+        }
+      }, []);
+
     return (
-        <div className="w-full h-full">
-            <div className='flex flex-col justify-center items-center h-full w-full mt-5 mb-5'>
-                <div className='slidebar flex flex-auto justify-center items-center h-96 w-1/2 mr-14'>
-                    Slide bar 1
-
-                </div>
-                <div className='content flex justify-center items-stretch h-full w-1/2 mt-5 mr-15 ml-5 '>
-
-                </div>
+        <div className="w-full h-full bg-gradient-to-b from-yellow-50 to-gray-100 p-6">
+            <div className="flex flex-col justify-center items-center w-full mt-5 text-4xl md:text-5xl lg:text-6xl text-[#8B4513]">
+                Chương 3
+            </div>
+            <div className="flex flex-col justify-center items-center text-center mt-2 text-4xl md:text-5xl lg:text-6xl text-[#8B4513] px-5 md:px-10 lg:px-20">
+                Tư tưởng Hồ Chí Minh về độc lập dân tộc và chủ nghĩa xã hội
+            </div>
+            <div className="flex justify-center mt-8" 
+                data-href="https://www.facebook.com/61566550163263/videos/440058908540797" 
+                data-width="10000" 
+                data-show-text="false">
+            </div>
+            <div className="flex justify-center mt-8">
+                <button 
+                    onClick={handleTestButtonClick}
+                    className="px-8 py-3 bg-[#8B4513] text-white text-xl rounded-lg shadow-lg hover:bg-[#7a3e12] transition-all duration-300"
+                >
+                    Làm bài quiz
+                </button>
             </div>
         </div>
     );
