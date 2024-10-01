b6a832c754846b2198e68aa39b8698a802ce8f46
Nguyễn Công Thịnh <thinh111190@gmail.com>
2024-10-01 16:03:52 +0700

    update2

--- src/pages/Chapter3Page.tsx
+++ src/pages/Chapter3Page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 declare global {
     interface Window {
@@ -13,9 +13,11 @@ const Chapter3Page: React.FC = () => {
         // Kiểm tra nếu SDK của Facebook đã được tải
         if (window.FB) {
           window.FB.XFBML.parse();
+          console.log(" run ===========================================");
         }
-      }, []);
-
+        setLinkFB("https://www.facebook.com/61566550163263/videos/440058908540797");
+      }, [window.FB])
+      const [linkFB, setLinkFB] = useState("https://www.facebook.com/61566550163263/videos/440058908540797");
     return (
         <div className="w-full h-full bg-gradient-to-b from-yellow-50 to-gray-100 p-6">
             <div className="flex flex-col justify-center items-center w-full mt-5 text-4xl md:text-5xl lg:text-6xl text-[#8B4513]">
@@ -24,11 +26,11 @@ const Chapter3Page: React.FC = () => {
             <div className="flex flex-col justify-center items-center text-center mt-2 text-4xl md:text-5xl lg:text-6xl text-[#8B4513] px-5 md:px-10 lg:px-20">
                 Tư tưởng Hồ Chí Minh về độc lập dân tộc và chủ nghĩa xã hội
             </div>
-            <div className="flex justify-center mt-8" 
-                data-href="https://www.facebook.com/61566550163263/videos/440058908540797" 
-                data-width="10000" 
+            <iframe className="flex justify-center mt-8" 
+                data-href={linkFB} 
+                data-width="500" 
                 data-show-text="false">
-            </div>
+            </iframe>
             <div className="flex justify-center mt-8">
                 <button 
                     onClick={handleTestButtonClick}
